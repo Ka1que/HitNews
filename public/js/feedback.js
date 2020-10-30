93e8411bb402e8538292f27d0c17abd0099181a6
Endobear <enriquemessias.souza@gmail.com>
2020-10-30 19:13:55 -0300

    Comentarios em alguns códigos e ícone >:V

--- public/js/feedback.js
+++ public/js/feedback.js
@@ -5,22 +5,28 @@ function validarFeedback() {
     //TODO Validação
     if (email_feedback.checkValidity()) {
         //Enviar feedback
-        let data = new Date();
-        let dataFormatada = data.getUTCDate() + "/" + (data.getUTCMonth() + 1) + "/" + data.getUTCFullYear();
+        let data = new Date();//pega a data
+        let dataFormatada = data.getUTCDate() + "/" + (data.getUTCMonth() + 1) + "/" + data.getUTCFullYear();//formata a data
         console.log(dataFormatada);
 
+        /**
+         * Objeto que vai ser mandado para o banco
+         */
         var feedback = {
             email: email_feedback.value,
             mensagem: document.getElementById("Text_opniao").value,
             data: dataFormatada
         }
 
+        /**
+         * manda o feedback para o banco e faz um promisse
+         */
         firebase.database().ref().child("feedbacks").push(feedback).then(() => {
-
-            document.getElementById("mensagem").innerHTML = "Agradecemos o Feedback!";
+            
+            document.getElementById("mensagem").innerHTML = "Agradecemos o Feedback!";//avisa para o usuário que o feedback foi enviado
         }).catch((erro) => {
-            document.getElementById("mensagem").innerHTML = "Erro ao enviar feedback!";
-            console.log(erro);
+            document.getElementById("mensagem").innerHTML = "Erro ao enviar feedback!";//avisa para o usuário que ocorreu um erro ao enviar o feedback
+            console.log(erro);//mostra o erro no console
         })
     }
 
--- public/js/perfil.js
+++ public/js/perfil.js
@@ -5,18 +5,24 @@
  */
 function carregar_perfil(perfil)
 {
+    //coloca banner do usuario na página
     let imagem_banner_perfil = document.getElementById("banner_usu");
     imagem_banner_perfil.src = perfil.banner;
 
+    //coloca imagem do usuário na página
     let imagem_usuario = document.getElementById("img_usu");
     imagem_usuario.src = perfil.fotoPerfil;
 
+    //coloca nome e sobrenome do usuário na página
     let nome_usuario = document.getElementById("nome_usu");
     nome_usuario.innerHTML = perfil.nome +" "+ perfil.sobrenome;
 
 
 }
 
+/**
+ * pega Id do usuário da url
+ */
 function getUsuId()
 {
     let url = window.location.href;
@@ -25,11 +31,18 @@ function getUsuId()
 
 }
 
+/**
+ * ativa quando os elementos DOM podem ser usados
+ */
 document.addEventListener("DOMContentLoaded", function() {
-   console.log(getUsuId());
+   //console.log(getUsuId());
+
+   /**
+    * Pega os dados do usuario e carrega o perfil
+    */
    firebase.database().ref("Usuarios").child(getUsuId()).once("value").then((result) => {
        
-    carregar_perfil(result.val());
+    carregar_perfil(result.val());//ativa a função para colocar o perfil na tela
 
    }).catch((err) => {
        
